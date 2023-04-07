e44f3d6a68031ac7db174637ac90184b6de7578b
schniti269 <116371884+schniti269@users.noreply.github.com>
2023-04-07 18:05:43 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,31 @@
+import java.util.ArrayList;
+import java.io.*;
+import java.util.concurrent.ThreadLocalRandom;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        File Folder =new File("Courses");
+        File file= new File(Folder.getPath()+File.separator+"Mathe.course");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        ArrayList tasks = new ArrayList<task>();
+        for(int i=0; i<10;i++){
+            int rtime=ThreadLocalRandom.current().nextInt(1, 7 + 1);
+            tasks.add(new task("Aufgabe"+i,rtime));
         }
+        course c2 = new course("Mathe",null,tasks);
+        c2.save();
+
+
+        course c = course.read(file);
+        c.sort();
+        c.Print();
+        c.DoTask(3);
+        c.Print();
+        c.name="prog2";
+        c.save();
     }
 }
